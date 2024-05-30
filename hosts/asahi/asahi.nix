1d6e30425f9169c0ec680a289fbb36aebb13993d
Moritz Gleissner <moritz@gleissner.de>
2024-05-30 17:31:34 +0200

    40 current  2024-05-30 17:31:31  24.11.20240527.9ca3f64  6.8.9-asahi                          *

--- hosts/asahi/asahi.nix
+++ hosts/asahi/asahi.nix
@@ -108,7 +108,7 @@
       input.kb_layout = "de";
       "$mod" = "SUPER";
       bind = ["$mod, return, exec, kitty"];
-      monitor = ["eDP-1, preferred, auto, 1"];
+      monitor = ["eDP-1,preferred,auto,1"];
     };
   };
 
