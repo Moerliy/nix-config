56608700ca9e5586e8eea40ba1df9fe292edbf27
Moritz Gleissner <moritz@gleissner.de>
2024-05-30 17:39:16 +0200

    43 current  2024-05-30 17:39:13  24.11.20240527.9ca3f64  6.8.9-asahi                          *

--- hosts/asahi/asahi.nix
+++ hosts/asahi/asahi.nix
@@ -60,7 +60,7 @@
     '';
     settings = {
       substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7YpFP8PwtkuGc="];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
   };
 
