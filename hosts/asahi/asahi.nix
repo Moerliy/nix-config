6a9a6f4d808800c93e8f8c4515647406c5778f10
Moritz Gleissner <moritz@gleissner.de>
2024-05-30 16:14:00 +0200

    24 current  2024-05-30 16:13:57  24.11.20240527.9ca3f64  6.8.9-asahi                          *

--- hosts/asahi/asahi.nix
+++ hosts/asahi/asahi.nix
@@ -78,6 +78,7 @@
   sane-defaults.enable = true;
   fish.enable = true;
   kitty.enable = true;
+  starship.enable = true;
 
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
