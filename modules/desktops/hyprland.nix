55bc94a7389ff5d745136e7009c88efd3031942e
Moritz Gleissner <moritz@gleissner.de>
2024-05-31 18:50:26 +0200

    54 current  2024-05-31 18:50:23  24.11.20240527.9ca3f64  6.8.9-asahi                          *

--- modules/desktops/hyprland.nix
+++ modules/desktops/hyprland.nix
@@ -268,21 +268,24 @@ with host; {
           animations = {
             enabled = false;
             bezier = [
+              "simple, 0.16, 1, 0.3, 1"
+              "tehtarik, 0.68, -0.55, 0.265, 1.55"
               "overshot, 0.05, 0.9, 0.1, 1.05"
-              "smoothOut, 0.5, 0, 0.99, 0.99"
-              "smoothIn, 0.5, -0.5, 0.68, 1.5"
+              "smoothOut, 0.36, 0, 0.66, -0.56"
+              "smoothIn, 0.25, 1, 0.5, 1"
               "rotate,0,0,1,1"
             ];
             animation = [
-              "windows, 1, 4, overshot, slide"
-              "windowsIn, 1, 2, smoothOut"
-              "windowsOut, 1, 0.5, smoothOut"
-              "windowsMove, 1, 3, smoothIn, slide"
-              "border, 1, 5, default"
-              "fade, 1, 4, smoothIn"
-              "fadeDim, 1, 4, smoothIn"
-              "workspaces, 1, 4, default"
-              "borderangle, 1, 20, rotate, loop"
+              "windows, 1, 5, overshot, popin"
+              "windowsIn, 1, 4, tehtarik, slide"
+              "windowsOut, 1, 4, tehtarik, slide"
+              # "windowsMove, 1, 3, smoothIn, slide"
+              # "border, 1, 5, default"
+              "fade, 1, 10, simple"
+              # "fadeDim, 1, 4, smoothIn"
+              "workspaces, 1, 6, simple, slide"
+              # "borderangle, 1, 20, rotate, loop"
+              "specialWorkspace, 1, 6, simple, slidevert"
             ];
           };
           input.kb_layout = "de";
