f3fd5b322a222fd89dac2b6037cf17d74128a5e1
Moritz Gleissner <moritz@gleissner.de>
2024-05-31 17:30:58 +0200

    48 current  2024-05-31 17:30:54  24.11.20240527.9ca3f64  6.8.9-asahi                          *

--- modules/desktops/hyprland.nix
+++ modules/desktops/hyprland.nix
@@ -118,56 +118,56 @@ with host; {
           hyprland.homeManagerModules.default
         ];
 
-        # programs.hyprlock = {
-        #   enable = true;
-        #   settings = {
-        #     general = {
-        #       hide_cursor = true;
-        #       no_fade_in = false;
-        #       disable_loading_bar = true;
-        #       grace = 0;
-        #     };
-        #     background = [
-        #       {
-        #         monitor = "";
-        #         path = "$HOME/.config/wall.png";
-        #         color = "rgba(25, 20, 20, 1.0)";
-        #         blur_passes = 1;
-        #         blur_size = 0;
-        #         brightness = 0.2;
-        #       }
-        #     ];
-        #     input-field = [
-        #       {
-        #         monitor = "";
-        #         size = "250, 60";
-        #         outline_thickness = 2;
-        #         dots_size = 0.2;
-        #         dots_spacing = 0.2;
-        #         dots_center = true;
-        #         outer_color = "rgba(0, 0, 0, 0)";
-        #         inner_color = "rgba(0, 0, 0, 0.5)";
-        #         font_color = "rgb(200, 200, 200)";
-        #         fade_on_empty = false;
-        #         placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
-        #         hide_input = false;
-        #         position = "0, -120";
-        #         halign = "center";
-        #         valign = "center";
-        #       }
-        #     ];
-        #     label = [
-        #       {
-        #         monitor = "";
-        #         text = "$TIME";
-        #         font_size = 120;
-        #         position = "0, 80";
-        #         valign = "center";
-        #         halign = "center";
-        #       }
-        #     ];
-        #   };
-        # };
+        programs.hyprlock = {
+          enable = true;
+          settings = {
+            general = {
+              hide_cursor = true;
+              no_fade_in = false;
+              disable_loading_bar = true;
+              grace = 0;
+            };
+            background = [
+              {
+                monitor = "";
+                path = "$HOME/.config/wallpaper.png";
+                color = "rgba(25, 20, 20, 1.0)";
+                blur_passes = 1;
+                blur_size = 0;
+                brightness = 0.2;
+              }
+            ];
+            input-field = [
+              {
+                monitor = "";
+                size = "250, 60";
+                outline_thickness = 2;
+                dots_size = 0.2;
+                dots_spacing = 0.2;
+                dots_center = true;
+                outer_color = "rgba(0, 0, 0, 0)";
+                inner_color = "rgba(0, 0, 0, 0.5)";
+                font_color = "rgb(200, 200, 200)";
+                fade_on_empty = false;
+                placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
+                hide_input = false;
+                position = "0, -120";
+                halign = "center";
+                valign = "center";
+              }
+            ];
+            label = [
+              {
+                monitor = "";
+                text = "$TIME";
+                font_size = 120;
+                position = "0, 80";
+                valign = "center";
+                halign = "center";
+              }
+            ];
+          };
+        };
         #
         # services.hypridle = {
         #   enable = true;
