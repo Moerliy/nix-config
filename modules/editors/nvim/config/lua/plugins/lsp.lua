b15110c3dc8ae7d93448c7c2e8bebd5aa973ba41
Moritz Gleissner <moritz@gleissner.de>
2024-06-04 16:48:57 +0200

    122 current  2024-06-04 16:48:52  24.11.20240527.9ca3f64  6.8.9-asahi                          *

--- modules/editors/nvim/config/lua/plugins/lsp.lua
+++ modules/editors/nvim/config/lua/plugins/lsp.lua
@@ -2,10 +2,10 @@ return {
   {
     "williamboman/mason.nvim",
     opts = {
-        registries = {
-          "github:mason-org/mason-registry",
-          "github:Moerliy/mason-registry",
-        },
+      registries = {
+        "github:mason-org/mason-registry",
+        -- "github:Moerliy/mason-registry",
+      },
       ensure_installed = {
         "stylua",
         "selene",
@@ -24,4 +24,27 @@ return {
       },
     },
   },
+  ---@param opts MasonSettings | {ensure_installed: string[]}
+  config = function(_, opts)
+    require("mason").setup(opts)
+    local mr = require("mason-registry")
+    mr:on("package:install:success", function()
+      vim.defer_fn(function()
+        -- trigger FileType event to possibly load this newly installed LSP server
+        require("lazy.core.handler.event").trigger({
+          event = "FileType",
+          buf = vim.api.nvim_get_current_buf(),
+        })
+      end, 100)
+    end)
+
+    mr.refresh(function()
+      for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
+    end)
+  end,
 }
