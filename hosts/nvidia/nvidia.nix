eec119b776193882a5deb4dedc2dcbb566ef05a0
Moritz Gleissner <moritz@gleissner.de>
2024-08-23 22:19:58 +0200

    16 current  2024-08-23 22:06:28  24.11.20240821.c374d94   6.6.47                          *

--- hosts/nvidia/nvidia.nix
+++ hosts/nvidia/nvidia.nix
@@ -113,10 +113,10 @@
   # Enable sound.
   # hardware.pulseaudio.enable = true;
   # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
 
   home-manager.users.${vars.user} = {
     home = {
